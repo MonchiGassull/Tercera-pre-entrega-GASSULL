e892fa3a2714eab14caf6b37bd8ab82f78c5a2ba
María Montserrat Gassull <montserratgassull@gmail.com>
2023-05-10 20:39:14 -0300

    agrgados vestidos y mallaas modelos

--- TiendaAnaRamona/models.py
+++ TiendaAnaRamona/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-class Producto(models.Model):
+class Malla(models.Model):
     nombre = models.CharField(max_length=256) #equivalente de str
     precio = models.IntegerField() #equivalente de int
     talle = models.CharField(max_length=256)
     
+class Vestidos(models.Model):
+    nombre = models.CharField(max_length=256) #equivalente de str
+    precio = models.IntegerField() #equivalente de int
+    talle = models.CharField(max_length=256)
+
 class Comprador(models.Model):
     nombre = models.CharField(max_length=256)
     apellido =models.CharField(max_length=256)
